054c4e97671377da8333c2eb9e2f2af96fe54b2a
saladman88 <sappasiriyingnork@gmail.com>
2025-06-10 20:53:08 +0700

    add Product

---
+++ src/components/Productcard.tsx
@@ -0,0 +1,21 @@
+import { createSignal } from "solid-js";
+
+export default function Productcard() {
+  const [count, setCount] = createSignal(0);
+  return (
+    <div class="card bg-base-100 w-96 shadow-sm">
+        <figure>
+            <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Card Title</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    </div>
+  );
+}
